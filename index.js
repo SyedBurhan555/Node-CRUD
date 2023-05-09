5fdbc9208a44be6c65b72edd076fda6f179d84e4
Syed Burhan <syedburhanali555@gmail.com>
2023-05-09 15:20:01 +0500

    feat/create-database-and-connect

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello burhan')
+const express = require('express');
+const app = express();
+const PORT =  8000;
+const dotenv = require("dotenv");
+
+dotenv.config({path:'./config.env'});
+require('./db/conn')
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at PORT ${PORT}`)
+})
+
+
+
